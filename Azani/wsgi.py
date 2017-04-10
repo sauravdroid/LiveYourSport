54ba2755a992b91520f8c59e8e09bf4554b77029
Saurav Biswas <saru.sreyo@gmail.com>
2017-04-11 02:40:21 +0530

    Second Commit

--- Azani/wsgi.py
+++ Azani/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Azani.settings")
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
